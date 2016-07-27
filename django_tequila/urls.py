3a8e671dc1368da2883bc1d3170d1f16ce2ef028
greg <charmier>
2016-07-27 09:30:55 +0200

    urlpatterns should be a plain list of django.conf.urls.url() instances instead. https://docs.djangoproject.com/fr/1.9/ref/urls/#django.conf.urls.url

--- django_tequila/urls.py
+++ django_tequila/urls.py
@@ -1,10 +1,8 @@
-try:
-    from django.conf.urls import patterns, url
-except ImportError:  # django < 1.4
-    from django.conf.urls.defaults import patterns, url
+from django.conf.urls import url
+from django_tequila.tequila_auth_views import login, logout, not_allowed
 
-urlpatterns = patterns('',
-    url(r'^login/?$', 'django_tequila.tequila_auth_views.login', name='login_view'),
-    url(r'^logout/$', 'django_tequila.tequila_auth_views.logout', name='logout'),
-    url(r'^not_allowed/$', 'django_tequila.tequila_auth_views.not_allowed', name='not_allowed'),
-)
+urlpatterns = [
+    url(r'^login/?$', login, name='login_view'),
+    url(r'^logout/$', logout, name='logout'),
+    url(r'^not_allowed/$', not_allowed, name='not_allowed'),
+]
